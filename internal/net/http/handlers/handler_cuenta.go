82af97e7127b0d764f7115fb15054696ef827a56
ramcan7 <m.ramos@pucp.edu.pe>
2025-05-26 01:03:08 -0500

    fix: se crea una cuenta dummy para luego poder traer los datos desde el admin

--- internal/net/http/handlers/handler_cuenta.go
+++ internal/net/http/handlers/handler_cuenta.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"fmt"
+
 	"github.com/MervanXD/backend_ClubYa/internal/api/models"
 	"github.com/MervanXD/backend_ClubYa/internal/models/cuenta"
 	"github.com/MervanXD/backend_ClubYa/logs"
@@ -33,4 +35,18 @@ func LogIn(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusInternalServerError).JSON(models.Error("Error al iniciar sesión", nil))
 	}
 	return c.Status(fiber.StatusOK).JSON(models.Succes("Inicio de sesión exitoso", cuentaMandar))
+}
+
+func CrearCuentaDummy(idTitular int) error {
+	var cuentaDTO cuenta.Cuenta
+	cuentaDTO.IdPersona = idTitular
+	cuentaDTO.Username = "dummy"
+	cuentaDTO.Contrasena = "dummy"
+	cuentaDTO.Email = "dummy@gmail.com"
+	err := cuenta.CrearCuenta(cuentaDTO)
+	if err != nil {
+		logs.Logger.Println("Error al crear cuenta dummy: ", err)
+		return fmt.Errorf("error al crear cuenta dummy")
+	}
+	return nil
 }
