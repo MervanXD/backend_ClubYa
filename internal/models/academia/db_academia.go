2702da74fd20d10b81e212bc8da8e4ed51194be8
JosueSB <a20206724@pucp.edu.pe>
2025-06-04 23:46:53 -0500

    Update db_academia.go

--- internal/models/academia/db_academia.go
+++ internal/models/academia/db_academia.go
@@ -2,20 +2,8 @@ package academia
 
 import (
 	"github.com/MervanXD/backend_ClubYa/database"
-	"github.com/MervanXD/backend_ClubYa/internal/models/tipos"
 	"github.com/MervanXD/backend_ClubYa/logs"
 )
-
-type AcademiaDTO struct {
-	ID          int64         `json:"id"`
-	Nombre      string        `json:"nombre"`
-	Descripcion string        `json:"descripcion"`
-	Deporte     tipos.Deporte `json:"deporte"`
-	Imagen      []byte        `json:"imagen"`
-	Monto       float64       `json:"monto"`
-	EdadMinima  int64         `json:"edad_minima"`
-	Inscritos   int64         `json:"cantidad_inscritos"`
-}
 
 func ObtenerAcademias() ([]AcademiaDTO, error) {
 	query := "call ingesoft.ListarAcademias()"
