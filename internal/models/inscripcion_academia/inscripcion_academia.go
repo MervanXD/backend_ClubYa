b7bc6a94e42b7ad0ae2b855a03c1c865ca33c3bf
JosueSB <a20206724@pucp.edu.pe>
2025-06-06 11:43:05 -0500

    Update inscripcion_academia.go

--- internal/models/inscripcion_academia/inscripcion_academia.go
+++ internal/models/inscripcion_academia/inscripcion_academia.go
@@ -1,8 +1,6 @@
 package inscripcionacademia
 
 import (
-	"time"
-
 	"github.com/MervanXD/backend_ClubYa/internal/models/academia"
 	grupoacademia "github.com/MervanXD/backend_ClubYa/internal/models/grupo_academia"
 	"github.com/MervanXD/backend_ClubYa/internal/models/persona"
@@ -12,7 +10,7 @@ import (
 
 type InscripcionAcademia struct {
 	IdInscripcionAcademia int                         `json:"id_inscripcion_academia"`
-	FechaInscripcion      time.Time                   `json:"fecha_inscripcion"`
+	FechaInscripcion      string                      `json:"fecha_inscripcion"`
 	EstadoInscripcion     tipos.Estado                `json:"estado"`
 	AcademiaInscrita      academia.Academia           `json:"academia"`
 	SocioInscrito         persona.Persona             `json:"socio"`
