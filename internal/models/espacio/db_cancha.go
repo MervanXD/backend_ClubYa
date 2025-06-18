c791bd3da53fdfa7284e7d427e7f2633df423a7b
JosueSB <a20206724@pucp.edu.pe>
2025-06-18 17:05:40 -0500

    Cambios para que los detalles de la disponibilidad incluyan el nombre de los socios

--- internal/models/espacio/db_cancha.go
+++ internal/models/espacio/db_cancha.go
@@ -117,7 +117,7 @@ func (r *canchaRespositoryDB) ActualizarParcial(id int, dto CanchaUpdateDTO) err
 	}
 
 	return nil
-
+}
 func (r *canchaRespositoryDB) ObtenerCanchasConfiguracion() ([]Cancha, error) {
 	query := "call ingesoft.ListarCanchasConfiguracion()"
 	rows, err := database.DB.Query(query)
