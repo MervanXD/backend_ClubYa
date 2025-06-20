89e68843a9bfa37434ea2e99a5895127836eb7a0
Alejandro Sanchez <a20202176@pucp.edu.pe>
2025-06-19 23:03:14 -0500

    Se agrego disponibilidad de bloques de hora de eventos por fecha

--- internal/models/detalle_disponibilidad/detalle_disponiblidad_repository.go
+++ internal/models/detalle_disponibilidad/detalle_disponiblidad_repository.go
@@ -6,4 +6,5 @@ type DetalleDisponibilidadRepository interface {
 	ActualizarEstadoDetalleDisponibilidad(idHorarioDia int, idBloqueTiempo int, estado string) (err error)
 	ObtenerDisponibilidadEspacioSocialPorId(ctx context.Context, idEspacio int, idHorarioDia int, idBloqueTiempo int) (*DisponibilidadEspacioResponse, error)
 	ObtenerDetalleDisponibilidadEspacioFechaId(idEspacio int, fecha string) ([]DetalleDisponibilidadDto, error)
+	ObtenerRangosInicioDisponibles(idEspacio int, fecha string) ([]string, error)
 }
