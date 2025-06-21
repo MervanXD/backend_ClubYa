645c359f599c3f9b2e70858ee004a2a81d1eef5e
MervanFMP <martin.morap@pucp.edu.pe>
2025-06-21 02:24:09 -0500

    se actualizo el actualizar disponibilidad

--- internal/models/detalle_disponibilidad/detalle_disponiblidad_repository.go
+++ internal/models/detalle_disponibilidad/detalle_disponiblidad_repository.go
@@ -3,7 +3,7 @@ package detalledisponibilidad
 import "context"
 
 type DetalleDisponibilidadRepository interface {
-	ActualizarEstadoDetalleDisponibilidad(idHorarioDia int, idBloqueTiempo int, estado string) (err error)
+	ActualizarEstadoDetalleDisponibilidad(etalle DetalleRequestActualizar) (err error)
 	ObtenerDisponibilidadEspacioSocialPorId(ctx context.Context, idEspacio int, idHorarioDia int, idBloqueTiempo int) (*DisponibilidadEspacioResponse, error)
 	ObtenerDetalleDisponibilidadEspacioFechaId(idEspacio int, fecha string) ([]DetalleDisponibilidadDto, error)
 	ObtenerRangosInicioDisponibles(idEspacio int, fecha string) ([]string, error)
