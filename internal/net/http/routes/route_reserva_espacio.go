a1dccc265386e248c53da264d9188a7371f2fdec
ramcan7 <m.ramos@pucp.edu.pe>
2025-06-22 15:13:53 -0500

    formato de codigo

--- internal/net/http/routes/route_reserva_espacio.go
+++ internal/net/http/routes/route_reserva_espacio.go
@@ -10,7 +10,7 @@ func RutasReservaEspacio(api fiber.Router) {
 	api.Post("/reserva-espacio-social", handlers.ReservarEspacioSocial)
 	api.Put("/anulacion-reserva-espacio-social", handlers.AnularReservarEspacioSocial)
 	api.Get("/reservas-espacios-social-socio/:idSocio", handlers.ListarEspaciosSocialesSocio)
-	api.Get("/reserva-canchas/:idSocio",handlers.ListarCanchasSocio)
+	api.Get("/reserva-canchas/:idSocio", handlers.ListarCanchasSocio)
 	api.Get("/reservas-espacios/:idEspacio", handlers.ListarReservasPorEspacio)
 	api.Put("/anulacion-reserva/aceptar", handlers.AceptarDevolucionAnulacionReserva)
 }
