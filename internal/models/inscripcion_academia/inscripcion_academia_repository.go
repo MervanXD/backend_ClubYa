b13dbe1ca0f56d0616e30628b0da0f01e687bf3f
MervanFMP <martin.morap@pucp.edu.pe>
2025-06-26 18:34:35 -0500

    listo el listar socios inscritos a la academia

--- internal/models/inscripcion_academia/inscripcion_academia_repository.go
+++ internal/models/inscripcion_academia/inscripcion_academia_repository.go
@@ -3,4 +3,5 @@ package inscripcionacademia
 type InscripcionAcademiaRepository interface {
 	RegistrarInscripcionAcademia(idPersonaint int, idGrupo int, idTarifa int, uniforme int, costo_total float64, idTitular int, metodoPago string) error
 	ObtenerFamiliaresInscritosAcademia(idSocio int) ([]InscritoAcademiaDTO, error)
+	ListarInscritosPorIdAcademia(idAcademia int) ([]InscritosAcademiaRequest, error)
 }
