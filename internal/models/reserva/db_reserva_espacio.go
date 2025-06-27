a1e8c84bb145927811e208a99a60020324082cdf
ramcan7 <m.ramos@pucp.edu.pe>
2025-06-26 19:04:00 -0500

    fixes para listar reservas de canchas y espacios

--- internal/models/reserva/db_reserva_espacio.go
+++ internal/models/reserva/db_reserva_espacio.go
@@ -127,7 +127,14 @@ func (r *reservaRepositoryDB) ObtenerReservasEspaciosSocialesSocio(idSocio int) 
 	var reservasSocio []ReservaEspacioSocialRequest
 	for rows.Next() {
 		var reserva ReservaEspacioSocialRequest
-
+		if err := rows.Scan(
+			&reserva.Id, &reserva.FechaReserva, &reserva.Fecha, &reserva.HoraInicio, &reserva.HoraFin,
+			&reserva.Estado, &reserva.IdHorarioDia, &reserva.Espacio.Id,
+			&reserva.Espacio.Codigo, &reserva.Espacio.Nombre, &reserva.Espacio.Ubicacion,
+			&reserva.Espacio.Capacidad, &reserva.Espacio.Costo, &reserva.Espacio.Actividad); err != nil {
+			logs.Logger.Println("Error al escanear el horario de la loza deportiva del socio: ", err)
+			return nil, err
+		}
 		reservasSocio = append(reservasSocio, reserva)
 	}
 	return reservasSocio, nil
@@ -146,7 +153,7 @@ func (r *reservaRepositoryDB) ObtenerReservasCanchasSocio(idSocio int) ([]Reserv
 		var reserva ReservaCanchaRequest
 		if err := rows.Scan(
 			&reserva.Id, &reserva.FechaReserva, &reserva.Fecha, &reserva.HoraInicio, &reserva.HoraFin,
-			&reserva.Estado, &reserva.IdBloqueTiempo, &reserva.IdHorarioDia, &reserva.Espacio.Id,
+			&reserva.Estado, &reserva.IdHorarioDia, &reserva.Espacio.Id,
 			&reserva.Espacio.Codigo, &reserva.Espacio.Nombre, &reserva.Espacio.Ubicacion,
 			&reserva.Espacio.Capacidad, &reserva.Espacio.Costo, &reserva.Espacio.Deporte); err != nil {
 			logs.Logger.Println("Error al escanear el horario de la loza deportiva del socio: ", err)
--- internal/models/reserva/espacio_social_socio_dto.go
+++ internal/models/reserva/espacio_social_socio_dto.go
@@ -6,14 +6,13 @@ import (
 )
 
 type ReservaEspacioSocialRequest struct {
-	Id             int                   `json:"id"`
-	Espacio        espacio.EspacioSocial `json:"espacioSocial"`
-	HoraInicio     string                `json:"horaInicio"`
-	HoraFin        string                `json:"horaFin"`
-	Fecha          string                `json:"fecha"`
-	FechaReserva   string                `json:"fechaReserva"`
-	Estado         tipos.Estado          `json:"estado"`
-	IdHorarioDia   int                   `json:"id_horario_dia"`
-	IdBloqueTiempo int                   `json:"id_bloque_tiempo"`
-	IdSocio        int                   `json:"id_persona"`
+	Id           int                   `json:"id"`
+	Espacio      espacio.EspacioSocial `json:"espacioSocial"`
+	HoraInicio   string                `json:"horaInicio"`
+	HoraFin      string                `json:"horaFin"`
+	Fecha        string                `json:"fecha"`
+	FechaReserva string                `json:"fechaReserva"`
+	Estado       tipos.Estado          `json:"estado"`
+	IdHorarioDia int                   `json:"id_horario_dia"`
+	IdSocio      int                   `json:"id_persona"`
 }
