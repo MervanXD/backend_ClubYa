11fbb0909c3c2c521724d36023e98b7fc4f2e0c1
ManuelSanchez125 <a20202176@pucp.edu.pe>
2025-07-04 00:19:01 -0500

    obtener idcuenta por idpersona

--- internal/models/cuenta/cuenta_repository.go
+++ internal/models/cuenta/cuenta_repository.go
@@ -4,4 +4,8 @@ type CuentaRepository interface {
 	CrearCuenta(cuenta Cuenta) (int64, error)
 	LogIn(cuenta Cuenta) (DTOCuenta, error)
 	CrearCuentaAdministrador(cuenta CuentaAdminDTO) error
+	ObtenerAdministradores() ([]CuentaAdminRequest, error)
+	ObtenerPerfilPorIdCuenta(idCuenta int64) (CuentaAdminDTO, error)
+	ActualizarCuentaAParcial(idCuenta int64, dto CuentaAdminUpdateDTO) error 
+	ObtenerIdCuentaPorPersona(idPersona int64) (int64, error)
 }
