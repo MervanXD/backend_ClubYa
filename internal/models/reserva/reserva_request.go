93b701bcbee9cc3a1ec5d5ad8d85e7fafd182ca0
JosueSB <a20206724@pucp.edu.pe>
2025-07-04 17:48:17 -0500

    Update reserva_request.go

--- internal/models/reserva/reserva_request.go
+++ internal/models/reserva/reserva_request.go
@@ -8,7 +8,8 @@ import (
 type ReservaRequest struct {
 	NombreSocio      string            `json:"nombre_socio"`
 	IdReserva        int               `json:"id_reserva"`
-	FechaReserva     utils.NullString  `json:"fecha"`
+	FechaReserva     utils.NullString  `json:"fecha_reserva"`   /*Fecha en la que se realizo la reserva*/
+	Fecha            utils.NullString  `json:"fecha_reservada"` /*Fecha de la reserva*/
 	HoraInicio       utils.NullString  `json:"hora_inicio"`
 	HoraFin          utils.NullString  `json:"hora_fin"`
 	Estado           tipos.Estado      `json:"estado"`
