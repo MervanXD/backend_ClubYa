deeebc0af794301bbcb88601805f8c2dfebf7486
ManuelSanchez125 <a20202176@pucp.edu.pe>
2025-07-13 20:35:47 -0500

    fix 2

--- internal/models/sesiones/db_sesiones.go
+++ internal/models/sesiones/db_sesiones.go
@@ -46,7 +46,6 @@ func (r *sesionRepositoryDB) ObtenerSesionesGrupo(idGrupo int) ([]Sesion, error)
 func (r *sesionRepositoryDB) InsertarSesionTx(tx *sql.Tx, sesion *Sesion) (int64, error) {
 	query := "CALL InsertarSesion(?,?,?,?)"
 	result, err := tx.Exec(query, sesion.IdGrupo, sesion.Dia.String(), sesion.HoraInicio, sesion.HoraFin)
-	fmt.Println(sesion.Dia)
 	if err != nil {
 		logs.Logger.Println("Error al insertar la sesión:", err)
 		return 0, err
